8c2c60aa1d565ecbc915d96c21902c2a24420d80
廖云富 <yunful@jumei.com>
2019-04-04 22:44:19 +0800

    remove sudo

--- docker-install.sh
+++ docker-install.sh
@@ -2,11 +2,11 @@
 
 # Update the apt package index:
 
-sudo apt-get update
+apt-get update
 
 # Install packages to allow apt to use a repository over HTTPS:
 
-sudo apt-get install \
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -15,15 +15,15 @@ sudo apt-get install \
 
 # Add Docker’s official GPG key:
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg |  apt-key add -
 
 # Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, by searching for the last 8 characters of the fingerprint.
 
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
 # Use the following command to set up the stable repository. To add the nightly or test repository, add the word nightly or test (or both) after the word stable in the commands below. 
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
@@ -33,11 +33,11 @@ sudo add-apt-repository \
 
 # Update the apt package index.
 
-sudo apt-get update
+apt-get update
 
 # Install the latest version of Docker CE and containerd, or go to the next step to install a specific version:
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io
 
 
 
